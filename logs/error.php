01765983b4ff3fe9c75b753393d3c287f4ded44a
MalakaGL <glmalaka@gmail.com>
2014-08-30 09:40:01 +0530

    Initial set up for finalizing

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-08-15 03:20:49 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2014-08-15	03:20:49	INFO	::1	Joomla FAILURE: 	Empty password not allowed
+2014-08-26	04:40:00	INFO	::1	Joomla FAILURE: 	Empty password not allowed
